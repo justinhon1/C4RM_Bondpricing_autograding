ce04daad3a85c94bf68489dc3000ee017e13e146
justinhon1 <70420643+justinhon1@users.noreply.github.com>
2020-09-27 13:33:21 -0400

    Initial commit

---
+++ math_func.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def add(x,y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def product(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
---
+++ test_math_func.py
@@ -0,0 +1,14 @@
+import math_func
+
+def test_add():
+    assert math_func.add(8,4) == 12
+
+def test_subtract():
+    assert math_func.subtract(8,4) == 4
+
+def test_product():
+    assert math_func.product(8,4) == 32
+
+def test_divide():
+    assert math_func.divide(8,4) == 2
+
