f7cfc58b8e8c9763ecdb044e931947af290454d9
justinhon1 <70420643+justinhon1@users.noreply.github.com>
2020-09-27 15:39:18 -0400

    Update bond_func.py

--- bond_func.py
+++ bond_func.py
@@ -7,5 +7,5 @@ This is a temporary script file.
 
 
 def Bond(y,face,couponRate,m,ppy=1):   
-    PV =((face*couponRate/ppy*(1-(1+y/ppy)**(-ppy*m)))/(y/ppy)) + face*(1+(y/ppy))**(-ppy*m)
+    ## Enter your coding here ##
     return PV
