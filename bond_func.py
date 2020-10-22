ae00014447f08b064da4bbd33e14e72423f341d9
David Romoff <DaveTheMovie@gmail.com>
2020-10-22 10:30:19 -0400

    Display clean-up

--- bond_func.py
+++ bond_func.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 
 def Bond(y,face,couponRate,m,ppy):   
-    ## Enter your coding here ##
+    ## Enter your code here ##
     return
