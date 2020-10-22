933e5560009c0f352aa229ab9a15675fc9895ba3
justinhon1 <70420643+justinhon1@users.noreply.github.com>
2020-10-22 19:47:36 -0400

    Update bond_func.py

--- bond_func.py
+++ bond_func.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-
-def Bond(y,face,couponRate,m,ppy):   
-    ## Enter your coding here ##
+def BondPricer(y,face,couponRate,m,ppy):   
+    ## Enter your code here ##
     return
